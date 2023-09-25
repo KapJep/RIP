4671d33be2560ded721d91e1a6e2352d583b8941
Theodore Mentis <th.mentis@gmail.com>
2023-09-25 19:42:03 +0300

    Updated jquery, use of promises (#31)
    * Updated jquery, use of promises
    
    * await async use to load results

--- www/js/rip.js
+++ www/js/rip.js
@@ -1,9 +1,9 @@
-﻿$( function(){
-	$( ".translatable" ).each( function(){
-		var str = $( this ).data( "transl" );
+﻿$(function () {
+	$(".translatable").each(function () {
+		var str = $(this).data("transl");
 		var translated_str = t(str);
-		if (this.id=="actor"){
-			$(this).attr("placeholder",translated_str);
+		if (this.id == "actor") {
+			$(this).attr("placeholder", translated_str);
 		}
 		else {
 			$(this).text(translated_str);
@@ -11,75 +11,98 @@
 	})
 })
 
-function anazitisi(){
-	//disable search button and change the text
-	document.getElementById("btnS").disabled = true;
-	document.getElementById("btnS").innerHTML = t("Searching...") ;
+function anazitisi() {
+	// Let's cache elements we access frequently for performance
+	var $btnS = $("#btnS");
+	var $actor = $("#actor");
+	var $list_group = $(".list-group");
 
-	//empty the list
-	$(".list-group").hide().empty();
+	// Disable search button and change the text
+	$btnS.attr("disabled", true);
+	$btnS.text(t("Searching..."));
+
+	// Empty the list
+	$list_group.hide().empty();
 
 	//get the search query and empty the field
-	actor = $("#actor").val();
-	$("#actor").val("");
+	const actor = $actor.val();
+	$actor.val("");
 
-	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.open("GET","https://api.themoviedb.org/3/search/person?api_key=d44e0ac5ceac0c812a5ffa8c7cc72ef0&query="+actor,true);
+	performSearch(actor, $list_group, $btnS);
 
-	xmlhttp.onreadystatechange=function() {
-		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-			r = eval ("(" + xmlhttp.responseText + ")");
 
-			var actor_array = r.results;
-			var len = actor_array.length;
-
-			console.log(len);
-			// to sosto einai r.total_results, alla prepei na vroume pos paei sto next page
-			if (len>0){
-				var i;
-				for (i=0;i<len;i++){
-
-					var id = actor_array[i].id;
-					var actor = get_actor(id);
-
-					if (actor.deathday == null) {
-						var new_span = $("<span class='badge bdg-width glyphicon glyphicon glyphicon-question-sign'> N/A</span>");
-					}
-					else if (actor.deathday.indexOf("-") >= 0) {
-						var new_span = $("<span class='badge bdg-width glyphicon glyphicon-ban-circle'> R.I.P.</span>");
-					}
-					else {
-						var new_span = $("<span class='badge bdg-width glyphicon glyphicon-heart'> ALIVE</span>");
-					}
-
-					var new_list_item = $("<li class='list-group-item'></li>");
-					var new_actor_name_link = $("<a target=_blank href='http://www.themoviedb.org/person/"+id+"'>"+actor.name+"</a>");
-					
-					$(new_list_item).appendTo(".list-group").append(new_span,new_actor_name_link);
-				}
-				$(".list-group").slideDown("slow");
-				document.getElementById("btnS").disabled = false;
-				document.getElementById("btnS").innerHTML = t("Search");
-			}
-		}
-	}
-	xmlhttp.send();
 	//enable search button again, wait for 10 sec and change the text
-	setTimeout(function(){
+	setTimeout(function () {
 		document.getElementById("btnS").disabled = false;
 		document.getElementById("btnS").innerHTML = "Αναζήτηση";
-	},10000);
+	}, 10000);
 }
 
-function get_actor (id) {
-	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.open("GET","https://api.themoviedb.org/3/person/"+id+"?api_key=d44e0ac5ceac0c812a5ffa8c7cc72ef0",false);
-	xmlhttp.send();
-	if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-		r = eval ("(" + xmlhttp.responseText + ")");
-		return r;
+// Θα προσπαθήσω να γράψω την performSearch με promises αντι για callbacks
+// τελικά χρησιμοποίησα το async/await γιατί είναι πιο εύκολο και κατανοητό
+async function performSearch(actor, $list_group, $btnS) {
+	const url = `https://api.themoviedb.org/3/search/person?api_key=d44e0ac5ceac0c812a5ffa8c7cc72ef0&query=${actor}`;
+
+	try {
+		const response = await fetch(url);
+		const data = await response.json();
+		const actor_array = data.results;
+		const len = actor_array.length;
+
+		if (len > 0) {
+			const promises = actor_array.map(async (actorInfo) => {
+				id = actorInfo.id;
+				const actor = await get_actor(id);
+				return actor;
+			});
+
+			const actors = await Promise.all(promises);
+
+			actors.forEach(actor => {
+				let new_span;
+				if (!actor.deathday || actor.deathday === "undefined") {
+					new_span = $("<span class='badge bdg-width glyphicon bi bi-question-circle'> N/A</span>");
+				} else if (actor.deathday.indexOf("-") >= 0) {
+					new_span = $("<span class='badge bdg-width glyphicon bi bi-slash-circle-fill bg-dark'> R.I.P.</span>");
+				} else {
+					new_span = $("<span class='badge bdg-width glyphicon bi bi-heart-fill bg-success'> ALIVE</span>");
+				}
+
+				const new_list_item = $("<li class='list-group-item'></li>");
+				const new_actor_name_link = $(`<a class="text-start" href = 'http://www.themoviedb.org/person/${actor.id}' target="_blank"> ${actor.name}</a> `);
+
+				new_list_item.append(new_span, new_actor_name_link);
+				$list_group.append(new_list_item);
+			});
+		}
+	} catch (error) {
+		console.error(error);
+	} finally {
+		$list_group.slideDown("slow");
+		$btnS.attr("disabled", false);
+		$btnS.text(t("Αναζήτηση"));
 	}
 }
+
+// Θα προσπαθήσω να γράψω την get_actor με async/await αντι για promises
+async function get_actor(id) {
+	const url = `https://api.themoviedb.org/3/person/${id}?api_key=d44e0ac5ceac0c812a5ffa8c7cc72ef0`;
+
+	try {
+		const response = await fetch(url);
+		const data = await response.json();
+		const actor = {
+			id: data.id,
+			name: data.name,
+			deathday: data.deathday ? data.deathday : "N/A"
+		};
+		return actor;
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
+}
+
 
 $("#actor").keypress(function (e) {
 	if (e.keyCode == 13) {
@@ -87,6 +110,6 @@ $("#actor").keypress(function (e) {
 	}
 })
 
-$("#btnS").click(function(){
+$("#btnS").click(function () {
 	anazitisi();
 })
